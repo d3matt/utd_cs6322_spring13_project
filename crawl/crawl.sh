fd9343ea4de67ba307760cc894ee4347f122a41c
Matthew Stoltenberg <d3matt@gmail.com>
2013-04-13 22:30:08 -0500

    better crawler, start of .pdf -> txt -> data

--- crawl/crawl.sh
+++ crawl/crawl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOS="J P N E S A D W C H I L O Y "
-OPTIONS="--no-host-directories --recursive --no-parent"
+OPTIONS="--no-host-directories --recursive --no-parent --timestamping"
 YEARS="00 01 02 03 04 05 06 07 08 09 10 11 12 13"
 
 for repo in $REPOS ; do
