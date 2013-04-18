b610d79108a2e8824df3ddcb077536b064778979
Matthew Stoltenberg <d3matt@gmail.com>
2013-04-17 22:06:45 -0500

    misc.
    updates to the model
    add porter.py from 3RD party
    updates to pre_index

--- index/cite/models.py
+++ index/cite/models.py
@@ -1,18 +1,31 @@
 from django.db import models
 
+#paper authors and citation authors may be in a different format
 class Paper_Author(models.Model):
-    name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, unique=True)
 
 class Citation_Author(models.Model):
-    name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, unique=True)
 
 class Citation(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     authors = models.ManyToManyField(Citation_Author)
     #if we want to put the other fields in the DB, add it here...
 
 class Paper(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     #still not sure if this relationship is the best...  C'est la vie
     authors = models.ManyToManyField(Paper_Author)
+    length = models.IntegerField()
     #if we want to put the abstract in the DB, add it here...
+
+class Token(models.Model):
+    stem = models.CharField(max_length=40, unique=True)
+    #not sure if we need this field, though it might be useful
+    total = models.IntegerField()
+
+class PaperToken(models.Model):
+    """many-many relationship of paper<->token with a count"""
+    num = models.IntegerField()
+    paper = models.ForeignKey(Paper)
+    token = models.ForeignKey(Token)
--- index/pre_index.py
+++ index/pre_index.py
@@ -1,20 +1,45 @@
 import json
+import os
+import re
 import xml.dom.minidom
 from xml.dom.minidom import parse, parseString
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "index.settings")
+import cite.models
+
+
+#third party stemmer
+import porter
+def stem(word):
+    return stem.stem(word, 0,len(word)-1)
+stem.stem = porter.PorterStemmer().stem
 
 class InvalidXml(Exception):
     pass
 
+def parse_cxml(filename):
+    dom = parse(filename + ".cxml")
+    citations = []
+    for citation in dom.getElementsByTagName("citation"):
+        attrs = citation.attributes
+        if (not attrs.has_key('valid')) or (attrs.get('valid').value != 'true'):
+            continue
+
+        citations.append(parse_citation(citation))
+    return citations
 
 def parse_citation(node):
-    simple_element_list = [ "date",
+    simple_element_list = ["title"]
+    ignore_element_list = [ "date",
                             "title",
                             "booktitle",
                             "pages",
                             "location",
                             "marker",
                             "journal",
-                            "rawString"
+                            "rawString",
+                            "contexts",
                             ]
     retval = {}
     for child in node.childNodes:
@@ -23,7 +48,7 @@ def parse_citation(node):
 
         if child.nodeName in simple_element_list:
             retval[child.nodeName] = child.childNodes[0].data
-        elif child.nodeName == "contexts":
+        elif child.nodeName in ignore_element_list:
             #don't know, don't care...
             pass
         elif child.nodeName == "authors":
@@ -39,20 +64,78 @@ def parse_citation(node):
             print "\t", child.nodeName
     return retval
 
-        
-    
+def parse_hxml(filename):
+    retval = {}
+    dom = parse(filename + ".hxml")    
+    title = dom.getElementsByTagName("title")[0].childNodes[0].data
+    authors = []
+    for node in dom.getElementsByTagName("author"):
+        for child in node.childNodes:
+            if not isinstance(child, xml.dom.minidom.Element):
+                continue
+            if child.nodeName == "name":
+                authors.append(child.childNodes[0].data)
+    return (title, authors)
 
 
-def parse_doc(filename):
-    dom = parse(filename + ".cxml")
-    citations = []
-    for citation in dom.getElementsByTagName("citation"):
-        attrs = citation.attributes
-        if (not attrs.has_key('valid')) or (attrs.get('valid').value != 'true'):
+STOP_WORDS = []
+def load_stopwords():
+    f = open( os.path.abspath(os.path.dirname(__file__)) +
+        "/../cite/HeaderParseService/resources/database/stopwords")
+    for line in f:
+        word = line.strip()
+        if word:
+            STOP_WORDS.append(word)
+load_stopwords()
+
+def parse_txt(filename):
+    """returns (length, stem:num_words) dict, mostly ripped from Matthew Stoltenberg's HW 1/2/3"""
+    words = open(filename + ".txt").read().split()
+    retval = {}
+    length = 0
+    for word in words:
+        orig = word
+        word = word.lower().strip()
+        #don't count whitespace
+        if len(word) == 0:
+            continue
+        #deal with digits
+        elif re.search("\d", word):
+            #if any alphabetical characters, do more:
+            if re.search("[a-zA-Z]", word):
+                word = re.sub("\d", "", word)
+                words.append(word)
+            continue
+        #deal with other characters
+        elif re.search("\W", word):
+            #allow possesives and contractions
+            if re.search("'s$", word):
+                pass
+            else:
+                for w in re.split("\W", word):
+                    words.append(w)
+                continue
+        length += 1
+        if word in STOP_WORDS or len(word) == 1:
             continue
 
-        citations.append(parse_citation(citation))
+        #stem the word
+        word = stem(word)
+        if word in retval:
+            retval[word] += 1
+        else:
+            retval[word] = 1
+
+    return (length, retval)
+
+def parse_doc(filename):
+    title, authors = parse_hxml(filename)
+    citations = parse_cxml(filename)
+    length, index = parse_txt(filename)
+    print title, length
+    print authors
     print json.dumps(citations, indent=2)
+    print index
 
 
 parse_doc("Y12-1005")
